a52c49a368efc13a59c9a7db7b1a74453bc1e698
John Doe <46622921+TheRandomOwl@users.noreply.github.com>
2024-06-04 13:13:56 -0700

    Added variables and functions

--- main.py
+++ main.py
@@ -1,34 +1,55 @@
+import numpy as np
 import os
 from gcodepy.gcode import Gcode
 
-build_dir = "build/"
-
-def rotate_gear(gcode, rate, amount):
-    if rate < 0:
-        raise ValueError("Rate must be positive")
-    gcode.file.write(f"G1 F{rate} E{amount}\n")
+# Constants
+PUSHER_LENGTH = 24
+FEEDER_CLEARANCE = 30
+FEEDER_WALL = 3.2
+TILE_SIZE = 12
+HOLE_DISTANCE = 16
+SAFE_RANGE = {
+    'x': {'MIN':-10, 'MAX':190},
+    'y': {'MIN':0.5, 'MAX':215},
+    'z': {'MIN':34.6, 'MAX':175}
+}
+BUILD_DIR = "build/"
 
 def setup(gcode):
-    gcode.file.write("G28 X Y\n") # home x and y
-    gcode.file.write("M302 S0\n") # allow extrusion without heating
+     # home print head
+    gcode.home()
+    gcode.file.write("M420 S1\n")
+
+    # center the print head
+    gcode.travel_absolute((89,115,SAFE_RANGE['z']['MIN']))
+    gcode.file.write("M0 S10 Press button to continue.\n")
+
+def end(gcode):
+    gcode.travel((0,0,FEEDER_CLEARANCE))
+    gcode.close()
+
+def despense_tile(gcode, tile_number):
+    pass
 
 def square_test(gcode):
-    gcode.travel_absolute((0,0,0))
-    gcode.travel_absolute((200,0,20))
-    gcode.travel_absolute((200,200,30))
-    gcode.travel_absolute((0,200,20))
-    rotate_gear(gcode, 70, -20)
-    gcode.travel_absolute((0,0,0))
+    gcode.travel_absolute((SAFE_RANGE['x']['MIN'],SAFE_RANGE['y']['MIN'],SAFE_RANGE['z']['MIN']))
+    gcode.travel((0,SAFE_RANGE['y']['MAX']-SAFE_RANGE['y']['MIN'],0))
+    gcode.travel((SAFE_RANGE['x']['MAX']-SAFE_RANGE['x']['MIN'],0,0))
+    gcode.travel((0,SAFE_RANGE['y']['MIN']-SAFE_RANGE['y']['MAX'],0))
+    gcode.travel((SAFE_RANGE['x']['MIN']-SAFE_RANGE['x']['MAX'],0,0))
+    # trave diagonally
+    gcode.travel((SAFE_RANGE['x']['MAX']-SAFE_RANGE['x']['MIN'],SAFE_RANGE['y']['MAX']-SAFE_RANGE['y']['MIN'],0))
 
 def main():
-    os.makedirs(build_dir, exist_ok=True)
-    g = Gcode(f"{build_dir}/test.gcode")
+    os.makedirs(BUILD_DIR, exist_ok=True)
+    g = Gcode(f"{BUILD_DIR}/test.gcode")
 
     setup(g)
     square_test(g)
-    g.close()
+    end(g)
     
-    with open(f"{build_dir}/test.gcode", "r") as f:
+    # print the gcode to the console
+    with open(f"{BUILD_DIR}/test.gcode", "r") as f:
         print(f.read())
 
     print("Done!")
